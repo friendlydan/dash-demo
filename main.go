57ffc90b1c4b0838b5ba0ae9a08d3b8b17541958
Daanikus <daanikus@gmail.com>
2019-08-15 09:57:38 +1200

    Export

--- main.go
+++ main.go
@@ -5,58 +5,58 @@ import (
 	"os"
 )
 
-func check(e error) {
+func Check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 func test1() *os.File {
 	f, err := os.Open("/tmp/test.txt")
-	check(err)
+	Check(err)
 	return f
 }
 
 func test2() {
 	f, err := os.Open("/tmp/test.txt") //ISSUE
-	check(err)
+	Check(err)
 	//defer f.Close()
 	b := make([]byte, 5)
 	n, err := f.Read(b)
-	check(err)
+	Check(err)
 	fmt.Printf("%d bytes: %s\n", n, string(b))
 }
 
 func test3() (f *os.File) {
 	f, err := os.Open("/tmp/test.txt")
-	check(err)
+	Check(err)
 	return
 }
 
 func main() {
 
 	f1, err := os.Open("/tmp/test.txt") //ISSUE
-	check(err)
+	Check(err)
 	//defer f1.Close()
 	b1 := make([]byte, 5)
 	n1, err := f1.Read(b1)
-	check(err)
+	Check(err)
 	fmt.Printf("%d bytes: %s\n", n1, string(b1))
 
 	f2, err := os.Open("/tmp/test.txt")
-	check(err)
+	Check(err)
 	defer f2.Close()
 	b2 := make([]byte, 5)
 	n2, err := f2.Read(b2)
-	check(err)
+	Check(err)
 	fmt.Printf("%d bytes: %s\n", n2, string(b2))
 	f2.Close()
 
 	f3, err := os.OpenFile("/tmp/test.txt", os.O_RDWR, 0644) //ISSUE
-	check(err)
+	Check(err)
 	//defer f3.Close()
 	b3 := make([]byte, 5)
 	n3, err := f3.Read(b3)
-	check(err)
+	Check(err)
 	fmt.Printf("%d bytes: %s\n", n3, string(b3))
 
 }
